e749731961f452212210c270c97a79929f55c232
berraozkan <berraozkan00@gmail.com>
2023-05-04 15:02:20 +0300

    blurry loading page

---
+++ script.js
@@ -0,0 +1,21 @@
+const loadingText = document.querySelector(".loading-text");
+const background = document.querySelector(".background");
+
+let load = 0;
+let int = setInterval(bluring, 90);
+
+function bluring() {
+    load++;
+
+    if (load > 99) {
+        clearInterval(int);
+    }
+
+    loadingText.innerText = `${load}%`;
+    loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+    background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+function scale(number, inMin, inMax, outMin, outMax) {
+    return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin
+}
